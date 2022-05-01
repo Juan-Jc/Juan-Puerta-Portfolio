6d10c4cf816db3102e9afe26fac221fbbf2c0482
Juan Puerta <juan.puerta1994@gmail.com>
2022-05-01 22:37:02 +0200

    updated components of header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -3,9 +3,9 @@ import {BsLinkedin, BsGithub,BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com'target='blank'><BsLinkedin /></a>
-        <a href='https://github.com'target='blank'><BsGithub /></a>
-        <a href='https://instagram.com'target='blank'><BsInstagram/></a>
+        <a href='https://linkedin.com'target='blank'><BsLinkedin color='var(--primary-color)' /></a>
+        <a href='https://github.com'target='blank'><BsGithub color='var(--primary-color)' /></a>
+        <a href='https://instagram.com'target='blank'><BsInstagram color='var(--primary-color)' /></a>
     </div>
   )
 }
