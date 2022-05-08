094cc388e7a8efe89cb0d72cec898fd1c8280bb8
Juan Puerta <juan.puerta1994@gmail.com>
2022-05-08 23:39:17 +0200

    fixed scrolldown color and blank of download

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/Curriculum.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download className='btn' target='_blank'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
