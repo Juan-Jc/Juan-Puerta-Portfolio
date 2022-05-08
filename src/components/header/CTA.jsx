785491013a4693da616d3bd8435885ba4bb42979
Juan Puerta <juan.puerta1994@gmail.com>
2022-05-08 23:27:46 +0200

    cv updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import CV from '../../assets/Curriculum.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='' download className='btn'>Download CV</a>
+        <a href={CV} download className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
